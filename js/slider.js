148abec29fe03a0be90c79fc3446eddbe5f31c4c
KaterinaLove <KaterinaLove@users.noreply.github.com>
2019-01-09 02:34:19 +0300

    Focus сохроняется

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,30 @@
-var imgNamber = 1,
-  slide = document.getElementById("s1");
+var slide1 = document.getElementById("s1"),
+    slide2 = document.getElementById("s2"),
+    slide3 = document.getElementById("s3"),
+    slide4 = document.getElementById("s4"),
+    slide5 = document.getElementById("s5"),
+    control1 = document.getElementById("control1"),
+    control2 = document.getElementById("control2"),
+    control3 = document.getElementById("control3"),
+    control4 = document.getElementById("control4"),
+    control5 = document.getElementById("control5");
 
-if (imgNamber < 5) {
-  setInterval(function () {
-    slide = document.getElementById("s+'imgNamber'");
-    imgNamber++;
-  }, 2000);
-} else {
-  imgNamber = 1;
+control1.onfocus = function() {
+//  if ()
+  control1.focus();
+  slide1.style.zIndex = "-1";
+  
 }
+control2.onfocus = function() {
+  slide2.style.zIndex = "-1";
+}
+control3.onfocus = function() {
+  slide3.style.zIndex = "-1";
+}
+control4.onfocus = function() {
+  slide4.style.zIndex = "-1";
+}
+control5.onfocus = function() {
+  slide5.style.zIndex = "-1";
+}
+
