b0f4be21bf8b277e5fd8ed1b11b030e6fd5d1cbd
KaterinaLove <KaterinaLove@users.noreply.github.com>
2019-01-10 03:20:35 +0300

    Оптимизировалла код

--- js/slider.js
+++ js/slider.js
@@ -5,13 +5,7 @@ var slide1 = document.getElementById("s1"),
   slide5 = document.getElementById("s5"),
   controlLeft = document.getElementById("controlLeft"),
   controlRight = document.getElementById("controlRight");
-
-var control1 = document.getElementById("control1"),
-  control2 = document.getElementById("control2"),
-  control3 = document.getElementById("control3"),
-  control4 = document.getElementById("control4"),
-  control5 = document.getElementById("control5");
-
+//кружочек пасивнай
 function pasiv(name) {
   let control = document.getElementById(name);
   control.style.borderWidth = "2px";
@@ -20,7 +14,7 @@ function pasiv(name) {
   control.style.borderRadius = "50%";
   control.style.backgroundColor = "#a4a3a1";
 }
-
+//кружочек активный
 function activ(name) {
   let control = document.getElementById(name);
   control.style.borderWidth = "2px";
@@ -29,7 +23,8 @@ function activ(name) {
   control.style.borderRadius = "50%";
   control.style.backgroundColor = "rgba(0, 0, 0, 0)";
 }
-
+//выбор слайда
+// Можно поменять на прозрачность и сделать плавные переходы
 function control(s1, s2, s3, s4, s5) {
   slide1.style.zIndex = s1;
   slide2.style.zIndex = s2;
@@ -78,8 +73,10 @@ function slid5() {
   pasiv("control4");
   pasiv("control1");
 }
-
-controlRight.onclick = function () {
+//Старт функции перваначальная позиция
+slid1();
+//При нажатии на правую стрелочку
+function slideRight() {
   if (slide1.style.zIndex == "-1") {
     slid2();
   } else if (slide2.style.zIndex == "-1") {
@@ -92,8 +89,8 @@ controlRight.onclick = function () {
     slid1();
   }
 }
-
-controlLeft.onclick = function () {
+//При нажатии левой стрелочки
+function slideLeft() {
   if (slide1.style.zIndex == "-1") {
     slid5();
   } else if (slide2.style.zIndex == "-1") {
@@ -106,26 +103,19 @@ controlLeft.onclick = function () {
     slid4();
   }
 }
-
+//При нажатии на ссылки
+controlLeft.onclick = function () {slideLeft();}
+controlRight.onclick = function() {slideRight();}
 control1.onclick = function() {slid1();}
 control2.onclick = function() {slid2();}
 control3.onclick = function() {slid3();}
 control4.onclick = function() {slid4();}
 control5.onclick = function() {slid5();}
+//Слайдер запускаеться после загрузки всей страницы
 document.addEventListener("DOMContentLoaded", timerId);
-
+//слайдер 6секунд
 function timerId() {
-  setInterval(function () {
-    if (slide1.style.zIndex == "-1") {
-      slid2();
-    } else if (slide2.style.zIndex == "-1") {
-      slid3();
-    } else if (slide3.style.zIndex == "-1") {
-      slid4();
-    } else if (slide4.style.zIndex == "-1") {
-      slid5();
-    } else if (slide5.style.zIndex == "-1") {
-      slid1();
-    }
-  }, 5000);
+  setInterval(function (){
+    slideRight();
+  }, 6000);
 }
